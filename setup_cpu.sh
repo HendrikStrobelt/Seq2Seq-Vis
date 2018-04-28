8e0c95edc44b04a25374f650dec2147a01d531ca
Hendrik Strobelt <HendrikStrobelt@users.noreply.github.com>
2018-04-28 13:24:06 -0400

    Update setup_cpu.sh
    force pytorch 0.3 for now. for #15

--- setup_cpu.sh
+++ setup_cpu.sh
@@ -3,7 +3,8 @@
 # Install all essential packages
 conda create --yes --name s2sv python=3.6 h5py numpy scikit-learn flask
 conda install --name s2sv --yes -c conda-forge connexion nodejs python-annoy
-conda install --name s2sv --yes -c pytorch pytorch torchvision faiss-cpu
+conda install pytorch=0.3.0 -c soumith
+conda install --name s2sv --yes -c pytorch faiss-cpu
 source activate s2sv
 
 
