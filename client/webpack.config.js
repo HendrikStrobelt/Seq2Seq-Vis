cf34ca7a0c19f6c5479d17cfc57f4962cd6ee679
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-04-28 15:38:27 +0200

    remove not needed import

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
