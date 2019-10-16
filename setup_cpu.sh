020ce6aa1825608ba28dbf1a4edd5799f3c63cfe
Leslie-Fang <Leslie-Fang@users.noreply.github.com>
2019-10-16 16:59:31 +0800

    fix the npm version problem
    New version of nodejs would cause the failure in npm install
    https://github.com/HendrikStrobelt/Seq2Seq-Vis/issues/29

--- setup_cpu.sh
+++ setup_cpu.sh
@@ -6,13 +6,13 @@
 # Install all essential packages
 conda create --yes --name s2sv python=3.6 h5py numpy scikit-learn flask
 conda install --name s2sv --yes -c conda-forge connexion nodejs python-annoy
-conda install --name s2sv --yes pytorch=0.3.1 -c soumith
+conda install --name s2sv --yes -c soumith pytorch=0.3.1
 conda install --name s2sv --yes -c pytorch faiss-cpu
 source activate s2sv
 
 
 # Build client
-conda install --name s2sv --yes -c conda-forge nodejs
+conda install --name s2sv --yes -c conda-forge nodejs==9.11.1
 cd client
 npm install
 npm run build
